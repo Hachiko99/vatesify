61140a266f05bb15b736632eec929bfe01fa0cf5
QRJ <qinanj22@gmail.com>
2022-09-04 18:27:42 -0400

    Initial commit

---
+++ src/main/groovy/hachiko99/vates/VatesApplication.groovy
@@ -0,0 +1,13 @@
+package hachiko99.vates
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class VatesApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(VatesApplication, args)
+	}
+
+}
